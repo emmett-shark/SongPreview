8782a9c9af876ce966a348dd41532db34a0bf240
Emmett <a>
2023-06-02 19:56:09 -0400

    v1.0.6 fix for TC 1.11C

--- Plugin.cs
+++ Plugin.cs
@@ -36,54 +36,21 @@ public class Plugin : BaseUnityPlugin
 
     public static IEnumerator Play(LevelSelectController __instance, string trackRef)
     {
-        var audioClipSync = GetClip(trackRef);
-        while (audioClipSync != null && audioClipSync.MoveNext())
-        {
-            yield return null;
-            if (audioClipSync?.Current is AudioClip clip)
-            {
-                __instance.bgmus.clip = clip;
-                __instance.bgmus.time = clip.length > 60 ? clip.length / 2 : 0;
-                __instance.bgmus.Play();
-            }
-        }
-    }
-
-    public static IEnumerator GetClip(string trackRef)
-    {
         var track = TrackLookup.lookup(trackRef);
         if (Globals.IsCustomTrack(trackRef))
         {
             var customTrack = track as CustomTrack;
-            return TrombLoader.Plugin.Instance.GetAudioClipSync(customTrack.folderPath + "/song.ogg");
+            var audioClipSync = TrombLoader.Plugin.Instance.GetAudioClipSync(customTrack.folderPath + "/song.ogg");
+            while (audioClipSync != null && audioClipSync.MoveNext())
+            {
+                yield return null;
+                if (audioClipSync?.Current is AudioClip clip)
+                {
+                    __instance.bgmus.clip = clip;
+                    __instance.bgmus.time = clip.length > 60 ? clip.length / 2 : 0;
+                    __instance.bgmus.Play();
+                }
+            }
         }
-        else
-        {
-            return GetBaseAudioSync(trackRef);
-        }
-    }
-
-    public static IEnumerator GetBaseAudioSync(string trackRef)
-    {
-        string baseSongPath = $"{Application.streamingAssetsPath}/trackassets/{trackRef}";
-        var bundleLoadRequest = AssetBundle.LoadFromFileAsync(baseSongPath);
-        yield return bundleLoadRequest;
-        while (!bundleLoadRequest.isDone)
-        {
-            yield return null;
-        }
-
-        var myLoadedAssetBundle = bundleLoadRequest.assetBundle;
-        var assetLoadRequest = myLoadedAssetBundle.LoadAssetAsync<GameObject>("music_" + trackRef);
-        yield return assetLoadRequest;
-        while (!assetLoadRequest.isDone)
-        {
-            yield return null;
-        }
-
-        GameObject prefab = assetLoadRequest.asset as GameObject;
-        yield return prefab.GetComponent<AudioSource>().clip;
-
-        myLoadedAssetBundle.Unload(false);
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Plays the song of the selected track in the middle
 - [BaboonAPI](https://github.com/tc-mods/BaboonAPI)
 
 ## Changelog:
+v1.0.6
+- Fix for TC 1.11C
+
 v1.0.5
 - Trombloader v2 compatibility
 - Play the full song if under a minute long
